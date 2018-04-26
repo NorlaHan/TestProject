385c7f15b018f0cdf39f4995a17b862dae93fe73
PhhP <ph_430@hotmail.com>
2018-04-26 18:31:10 +0800

    Using CrossPlatformInputManager

---
+++ Assets/Player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (CrossPlatformInputManager.GetButtonDown("Vertical")) {
+			if (CrossPlatformInputManager.GetAxisRaw("Vertical") > 0) {
+				Debug.Log ("W is pressed");
+			}else{
+				Debug.Log ("S is pressed");
+			}
+		}else if (CrossPlatformInputManager.GetButtonDown("Horizontal")) {
+			if (CrossPlatformInputManager.GetAxisRaw("Vertical")>0) {
+				Debug.Log ("D is pressed");
+			}else{
+				Debug.Log ("A is pressed");
+			}
+		}
+	}
+}
